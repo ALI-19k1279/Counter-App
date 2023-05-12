e3c5689b03b9072e0781dc103d6741a08f6281c9
Muhammad Ali Rehman <alirehman@pk.folio3.com>
2023-05-12 11:20:31 +0500

    added counter component

---
+++ src/components/TheCounter.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'TheCounter',
+  setup() {
+    const count = ref<number>(0)
+    const increment = () => {
+      count.value++
+    }
+    const decrement = () => {
+      if (count.value != 0) count.value--
+    }
+    return {
+      count,
+      increment,
+      decrement
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="counter">
+    <button @click="decrement" class="counter-button">-</button>
+    <span class="counter-value">{{ count }}</span>
+    <button @click="increment" class="counter-button">+</button>
+  </div>
+</template>
+
+<style scoped>
+.counter {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.counter-button {
+  background-color: transparent;
+  border: 3px solid #333;
+  color: #e2e2e2;
+  font-size: 1.2rem;
+  padding: 0.5rem 1rem;
+  border-radius: 999px;
+  cursor: pointer;
+  transition: transform 0.3s, color 0.3s;
+}
+
+.counter-button:hover {
+  background-color: #41b883;
+}
+
+.counter-value {
+  font-size: 2rem;
+  margin: 0 1rem;
+  color: #34495e;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheCounter from '../components/TheCounter.vue'
+</script>
+
+<template>
+  <main>
+    <TheCounter />
+  </main>
+</template>
